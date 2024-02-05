be32f318459d03668c7f4bc8717f39cb3821999f
Kathurima Kimathi <kathurimakimathi415@gmail.com>
2024-02-05 21:37:55 +0300

    feat: setup hummingbird page
    Signed-off-by: Kathurima Kimathi <kathurimakimathi415@gmail.com>

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// Specify the path to your .env file
+const envFilePath = path.resolve(__dirname, '/home/kathurima/Documents/Personal/Credentials', '.env');
+
+// Load environmental variables from the .env file
+dotenv.config({ path: envFilePath });
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const username = process.env.ATLAS_USERNAME;
+const passphrase = process.env.ATLAS_PASSWORD;
+
+const dbURI = "mongodb+srv://" + username + ":" + passphrase + "@ninjatuts.kvjscb4.mongodb.net/node-auth?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/hummingbirds', (req, res) => res.render('hummingbirds'));
+
+app.use(authRoutes)
